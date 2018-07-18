eae4225739f3a80f602402ad151abee004b9e8e4
Peter Roth <proth@vnomicscorp.com>
2018-07-18 09:13:14 -0400

    Improves quick mod tap presses
    This allows faster typing of layer switch keys, this may have downside
    of poor typing causing unwanted layer switch if key is incorrectly held.
    Time will tell if this is ok.

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -45,8 +45,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
-#define TAPPING_TERM    200
+#define TAPPING_TERM    125
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+
+//This with lower TAPPING_TERM results in less missed *presses* on mod tap keys that are not activating when typing
+//mod tap keys quickly
+#define RETRO_TAPPING
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
